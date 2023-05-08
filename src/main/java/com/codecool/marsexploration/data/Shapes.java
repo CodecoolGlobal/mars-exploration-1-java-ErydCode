ef0d8471f75c1db62935b85b2fbcf4fb78f4268b
Andras <kovalcsik.andras87@gmail.com>
2023-05-08 15:23:15 +0200

    starrt with architecture

--- src/main/java/com/codecool/marsexploration/data/Shapes.java
+++ src/main/java/com/codecool/marsexploration/data/Shapes.java
@@ -1,18 +1,22 @@
 package com.codecool.marsexploration.data;
 
 public enum Shapes {
-    MOUNTAIN("^",40),
-    PIT("#",20),
-    MINERAL("*",10),
-    WATER("~",10),
-    EMPTY(" ",20);
+    MOUNTAIN("^",40, 6),
+    PIT("#",20, 6),
+    MINERAL("*",10, 7),
+    WATER("~",10, 5),
+    EMPTY(" ",20, 15);
 
     private final double totalAreaUsed;
     private final String shape;
+    private final int size;
 
-    Shapes(String shape, double totalAreaUsed) {
+
+
+    Shapes(String shape, double totalAreaUsed, int size) {
         this.shape = shape;
         this.totalAreaUsed = totalAreaUsed;
+        this.size = size;
     }
 
     public double getTotalAreaUsed(){
--- src/main/java/com/codecool/marsexploration/logic/MapConfig.java
+++ src/main/java/com/codecool/marsexploration/logic/MapConfig.java
@@ -3,7 +3,4 @@ package com.codecool.marsexploration.logic;
 public class MapConfig {
 
 
-    public String returnX (String x) {
-    return x;
-    }
 }
