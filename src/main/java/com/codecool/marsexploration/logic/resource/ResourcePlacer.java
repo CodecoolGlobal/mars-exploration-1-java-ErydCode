e4560162388981b4714df4edac93d2f84405eb50
ErydCode <dyre.espana@gmail.com>
2023-05-11 21:24:07 +0200

    ResourcePlacer works check only 1ToDo

--- src/main/java/com/codecool/marsexploration/logic/resource/ResourcePlacer.java
+++ src/main/java/com/codecool/marsexploration/logic/resource/ResourcePlacer.java
@@ -12,111 +12,110 @@ import java.util.stream.Collectors;
 
 public class ResourcePlacer {
     public String[][] placeInTerrain(String[][] planetTerrains, Planet planet, Random random, Display display) {
-        String[][] finalTerrain = planetTerrains;
-        System.out.println(Arrays.deepToString(finalTerrain));
-        display.printEndLines();
+        //ToDo Maybe Small Bug: by creating Map preference Symbol from Resource have to be equal to Area Symbol with the Same Index!
+        Map<String, Integer> mapOfAreaSymbolAndAmount = getMapOfAreasAndTotalSymbolsOnPlanet(planetTerrains, planet);
+        Map<String, Integer> mapOfResourcesAndAmount = getMapOfResourcesAndAmount(planet, random, display, mapOfAreaSymbolAndAmount);
+        getRandomStartCoordinateToPlaceResource(planetTerrains, random, mapOfAreaSymbolAndAmount, mapOfResourcesAndAmount);
+        return planetTerrains;
+    }
 
-        //ToDo Slap
-        Map<String, Integer> mapOfAreaSymbolAndAmount = planet.resources().stream()
-                .flatMap(resource -> planet.areas().stream()
-                        .filter(area -> resource.preferences().equals(area.symbol()))
-                        .flatMap(area -> Arrays.stream(finalTerrain)
-                                .flatMap(Arrays::stream)
-                                .filter(terrain -> terrain.equals(area.symbol()))))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
-        System.out.println("\nMap of Area: " + mapOfAreaSymbolAndAmount);
-        System.out.println("Size of Map of Area: " + mapOfAreaSymbolAndAmount.size());
-        display.printEndLines();
 
-        //ToDo Slap
+    private Map<String, Integer> getMapOfResourcesAndAmount(Planet planet, Random random, Display display, Map<String, Integer> mapOfAreaSymbolAndAmount) {
         Map<String, Integer> mapOfResourcesAndAmount = new HashMap<>();
         for (int i = 0; i < planet.resources().size(); i++) {
             String actualAreaSymbol = mapOfAreaSymbolAndAmount.keySet().toArray()[i].toString();
             int totalAmountOfArea = mapOfAreaSymbolAndAmount.get(actualAreaSymbol);
             String actualResourceSymbol = planet.resources().get(i).symbol();
+            String actualPreferenceSymbol = planet.resources().get(i).preferences();
             int resourceAmount = 0;
-            if (actualResourceSymbol.equals(actualAreaSymbol)) {
-                int minimumDivisor = 4;
-                if (minimumDivisor < totalAmountOfArea) {
-                    resourceAmount = totalAmountOfArea / random.nextInt(4, totalAmountOfArea);
-                } else {
-                    while (minimumDivisor > 0) {
-                        minimumDivisor--;
-                    }
-                    display.errorMessage("Your Size of this Area is 0! Please make a new Planet with minim Area Size of 1");
-                }
-            } else {
-                int areasAmount = planet.amountAreas();
-                if (areasAmount > 0) {
-                    resourceAmount = planet.amountAreas() / random.nextInt(1, planet.amountAreas());
-                } else {
-                    display.errorMessage("Your Planet hase no Areas! Please create a new Planet with minim 1 Area");
-                }
-            }
+            resourceAmount = getResourceAmount(planet, random, display, actualAreaSymbol, totalAmountOfArea, actualPreferenceSymbol, resourceAmount);
             mapOfResourcesAndAmount.put(actualResourceSymbol, resourceAmount);
         }
+        return mapOfResourcesAndAmount;
+    }
 
+    private int getResourceAmount(Planet planet, Random random, Display display, String actualAreaSymbol, int totalAmountOfArea, String actualPreferenceSymbol, int resourceAmount) {
+        if (!actualPreferenceSymbol.equals(actualAreaSymbol)) {
+            int minimumDivisor = 4;
+            if (minimumDivisor < totalAmountOfArea) {
+                resourceAmount = totalAmountOfArea / random.nextInt(minimumDivisor, (totalAmountOfArea / 2));
+            } else {
+                while (minimumDivisor > 0) {
+                    minimumDivisor--;
+                }
+                display.errorMessage("Your Size of this Area is 0! Please make a new Planet with minim Area Size of 1");
+            }
+        } else {
+            int areasAmount = planet.amountAreas();
+            if (areasAmount > 0) {
+                resourceAmount = planet.amountAreas() / random.nextInt(1, planet.amountAreas());
+            } else {
+                display.errorMessage("Your Planet hase no Areas! Please create a new Planet with minim 1 Area");
+            }
+        }
+        return resourceAmount;
+    }
 
+    private void getRandomStartCoordinateToPlaceResource(String[][] planetTerrains, Random random, Map<String, Integer> mapOfAreaSymbolAndAmount, Map<String, Integer> mapOfResourcesAndAmount) {
         int counterResource = 0;
         while (counterResource < mapOfResourcesAndAmount.size()) {
             int counterAmountOfResource = 0;
             String actualKeyOfResource = mapOfResourcesAndAmount.keySet().toArray()[counterResource].toString();
             while (counterAmountOfResource < mapOfResourcesAndAmount.get(actualKeyOfResource)) {
-                int randomY = random.nextInt(finalTerrain.length);
-                int randomX = random.nextInt(finalTerrain.length);
+                int randomY = random.nextInt(planetTerrains.length);
+                int randomX = random.nextInt(planetTerrains.length);
                 String actualKeyOfArea = mapOfAreaSymbolAndAmount.keySet().toArray()[counterResource].toString();
-                while (!finalTerrain[randomY][randomX].equals(actualKeyOfArea)) {
-                    randomY = random.nextInt(finalTerrain.length);
-                    randomX = random.nextInt(finalTerrain.length);
+                while (!planetTerrains[randomY][randomX].equals(actualKeyOfArea)) {
+                    randomY = random.nextInt(planetTerrains.length);
+                    randomX = random.nextInt(planetTerrains.length);
                 }
-                System.out.println("Map of Resource: " + mapOfResourcesAndAmount);
-                System.out.println("Size of Map of Resource: " + mapOfResourcesAndAmount.size());
-                display.printEndLines();
-
-                //ToDo Slap
-                System.out.println("\nRandomY to find empty space Start at Area: " + randomY);
-                System.out.println("RandomX  to find empty space Start at Area: " + randomX);
-                String actualTerrain = finalTerrain[randomY][randomX];
-                while (actualTerrain.equals(actualKeyOfArea) &&
-                        !actualTerrain.equals(" ")) {
-                    System.out.println("Actual Terrain searching: " + actualTerrain);
-                    System.out.println("Actual Area Symbol to check while searching: " + actualKeyOfArea);
-                    int randomPlacer = random.nextInt(1, 4);
-                    System.out.println("randomPlacer to find empty place beside preference: " + randomPlacer);
-                    actualTerrain = finalTerrain[randomY][randomX];
-                    switch (randomPlacer) {
-                        case 1 -> {
-                            if (randomX < planet.xyLength()) {
-                                randomX++;
-                            }
-                        }
-                        case 2 -> {
-                            if (randomX > 0) {
-                                randomX--;
-                            }
-                        }
-                        case 3 -> {
-                            if (randomY < planet.xyLength()) {
-                                randomY++;
-                            }
-                        }
-                        case 4 -> {
-                            if (randomY > 0) {
-                                randomY--;
-                            }
-                        }
-                    }
-                }
-                System.out.println("\nTerrainXY to Print: " + planetTerrains[randomY][randomX]);
-                System.out.println("Is place empty: " + planetTerrains[randomY][randomX].equals(" "));
-                if (planetTerrains[randomY][randomX].equals(" ")) {
-                    System.out.println("Resource To Print: " + actualKeyOfResource);
-                    finalTerrain[randomY][randomX] = actualKeyOfResource;
-                }
+                getValidatedResourceCoordinate(planetTerrains, random, actualKeyOfResource, randomY, randomX, actualKeyOfArea);
                 counterAmountOfResource++;
             }
             counterResource++;
         }
-        return finalTerrain;
+    }
+
+    private Map<String, Integer> getMapOfAreasAndTotalSymbolsOnPlanet(String[][] planetTerrains, Planet planet) {
+        return planet.resources().stream()
+                .flatMap(resource -> planet.areas().stream()
+                        .filter(area -> resource.preferences().equals(area.symbol()))
+                        .flatMap(area -> Arrays.stream(planetTerrains)
+                                .flatMap(Arrays::stream)
+                                .filter(terrain -> terrain.equals(area.symbol()))))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+    }
+
+    private void getValidatedResourceCoordinate(String[][] planetTerrains, Random random, String actualKeyOfResource, int randomY, int randomX, String actualKeyOfArea) {
+        String actualTerrain;
+        do {
+            int randomPlacer = random.nextInt(1, 4);
+            actualTerrain = planetTerrains[randomY][randomX];
+            switch (randomPlacer) {
+                case 1 -> {
+                    if (randomX < planetTerrains.length - 1) {
+                        randomX++;
+                    }
+                }
+                case 2 -> {
+                    if (randomX > 0) {
+                        randomX--;
+                    }
+                }
+                case 3 -> {
+                    if (randomY < planetTerrains.length - 1) {
+                        randomY++;
+                    }
+                }
+                case 4 -> {
+                    if (randomY > 0) {
+                        randomY--;
+                    }
+                }
+            }
+            if (planetTerrains[randomY][randomX].equals(" ")) {
+                planetTerrains[randomY][randomX] = actualKeyOfResource;
+            }
+        } while (actualTerrain.equals(actualKeyOfArea) && !actualTerrain.equals(" "));
     }
 }
