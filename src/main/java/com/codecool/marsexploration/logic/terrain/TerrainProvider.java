f8d1f8116f73a974ca43d85a27b8c0a6ba3c3f77
ErydCode <dyre.espana@gmail.com>
2023-05-11 11:55:42 +0200

    Initial service class to TerrainProvider

--- src/main/java/com/codecool/marsexploration/logic/terrain/TerrainProvider.java
+++ src/main/java/com/codecool/marsexploration/logic/terrain/TerrainProvider.java
@@ -2,6 +2,7 @@ package com.codecool.marsexploration.logic.terrain;
 
 import com.codecool.marsexploration.data.Area;
 import com.codecool.marsexploration.data.Planet;
+import com.codecool.marsexploration.logic.resource.ResourcePlacer;
 import com.codecool.marsexploration.ui.Display;
 
 import java.util.List;
@@ -9,16 +10,17 @@ import java.util.Random;
 
 public class TerrainProvider {
     private final Random random;
+    //ToDo initial this classes down below just one time put all in Application and get it per Parameter or Constructor!
     private final TerrainValidator terrainValidator = new TerrainValidator();
+    private final ResourcePlacer resourcePlacer = new ResourcePlacer();
 
     public TerrainProvider(Random random) {
         this.random = random;
     }
 
     public String[][] randomGenerated(Planet planet, Display display) {
-            //[y][x]
+        //[y][x]
         String[][] planetTerrains = new String[planet.xyLength()][planet.xyLength()];
-
         while (terrainValidator.isEmptySpace()) {
             int areaCounter = 0;
             while (areaCounter < planet.amountAreas()) {
@@ -33,6 +35,7 @@ public class TerrainProvider {
                 }
                 areaCounter++;
             }
+            resourcePlacer.placeInTerrain(planetTerrains, planet, random, display);
         }
         display.errorMessage("The planet does not have enough space for all areas, please reduce your number of areas");
         return planetTerrains;
