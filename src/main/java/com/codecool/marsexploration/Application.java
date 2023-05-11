7c0e16bbda781809c210f128d74bc8178888d38e
ErydCode <dyre.espana@gmail.com>
2023-05-11 11:56:13 +0200

    Add Option to Create Planet or use Preset

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -12,11 +12,19 @@ public class Application {
         Display display = new Display();
         Input input = new Input(display);
         Random random = new Random();
-        display.printTitle("Welcome to planet creator - simulate your planet");
         PlanetProvider planet = new PlanetProvider(display, input, random);
-        Planet mars = planet.getPlanet();
-        display.printTitle(mars.name());
-        System.out.println("RAW Data from " + mars.name() + ": " + mars);
+        display.printTitle("Welcome to planet creator - simulate your planet");
+        display.printSubtitle("You want create your own Planet or use an already explored Exodus Planet?");
+        int userChoice = 0;
+        while (userChoice < 1 || userChoice > 2) {
+            userChoice = input.getNumericUserInput("Please enter 1 to create your own Planet\n" +
+                    "Please enter 2 to use the explored Exodus Planet");
+        }
+        if (userChoice == 1) {
+            Planet createdPlanet = planet.getPlanet();
+            display.printTitle(createdPlanet.name());
+            System.out.println("RAW Data from " + createdPlanet.name() + ": " + createdPlanet);
+        }
         display.printEndLines();
     }
 }
