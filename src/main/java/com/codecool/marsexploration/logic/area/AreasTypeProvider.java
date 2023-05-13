e97972610ad3e778d3f97006e47da7bb742a4be8
ErydCode <dyre.espana@gmail.com>
2023-05-14 01:50:07 +0200

    rename getTerrain to getArea

--- src/main/java/com/codecool/marsexploration/logic/area/AreasTypeProvider.java
+++ src/main/java/com/codecool/marsexploration/logic/area/AreasTypeProvider.java
@@ -9,12 +9,12 @@ import java.util.Random;
 
 public class AreasTypeProvider {
 
-    public List<Area> getTerrain(String name, int amount, int minSize, int maxSize, String symbol, int[] rgb, Random random) {
+    public List<Area> getArea(String name, int amount, int minSize, int maxSize, String symbol, int[] rgb, Random random) {
         List<Area> areas = new ArrayList<>();
         for (int i = 1; i <= amount; i++) {
             int randomNumber = random.nextInt(minSize, maxSize);
             areas.add(new Area(i + "." + name, randomNumber, symbol, rgb));
         }
-        return areas.stream().sorted(Comparator.comparingInt(Area::size)).toList();
+        return areas.stream().sorted(Comparator.comparingInt(Area::amountOfSymbols)).toList();
     }
 }
