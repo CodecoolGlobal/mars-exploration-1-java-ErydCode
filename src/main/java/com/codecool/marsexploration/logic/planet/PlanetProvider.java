b7c7e35a258dc9ecd2ae29a381c93126f0209f27
ErydCode <dyre.espana@gmail.com>
2023-05-14 01:52:01 +0200

    rework dependency injection

--- src/main/java/com/codecool/marsexploration/logic/planet/PlanetProvider.java
+++ src/main/java/com/codecool/marsexploration/logic/planet/PlanetProvider.java
@@ -4,7 +4,7 @@ import com.codecool.marsexploration.data.Area;
 import com.codecool.marsexploration.data.Planet;
 import com.codecool.marsexploration.data.Resource;
 import com.codecool.marsexploration.logic.area.AreasProvider;
-import com.codecool.marsexploration.logic.resource.ResourceProvider;
+import com.codecool.marsexploration.logic.resource.ResourcesProvider;
 import com.codecool.marsexploration.ui.Display;
 import com.codecool.marsexploration.ui.Input;
 
@@ -16,13 +16,15 @@ public class PlanetProvider {
     private final Display display;
     private final Input input;
     private final Random random;
-    private final AreasProvider areas = new AreasProvider();
-    private final ResourceProvider resource = new ResourceProvider();
+    private final AreasProvider areas;
+    private final ResourcesProvider resource;
 
-    public PlanetProvider(Display display, Input input, Random random) {
+    public PlanetProvider(Display display, Input input, Random random, AreasProvider areas, ResourcesProvider resource) {
         this.display = display;
         this.input = input;
         this.random = random;
+        this.areas = areas;
+        this.resource = resource;
     }
 
     public Planet getPlanet() {
@@ -30,7 +32,7 @@ public class PlanetProvider {
         String name = input.getUserInput("Please enter the name of the planet.");
         int xyLength = input.getNumericUserInput("Please enter length of the planet.");
         int amountAreas = input.getNumericUserInput("Please enter how many areas the planet have.");
-        List<Area> allAreas = areas.getAreas(display, input, random);
+        List<Area> allAreas = areas.getAreas();
         List<Resource> allResource = resource.getResource(display, input, allAreas);
         return new Planet(name, xyLength, allAreas, amountAreas, allResource);
     }
