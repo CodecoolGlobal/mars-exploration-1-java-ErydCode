f74af8d6dfded8e8ff53c303ad7141bce521e110
ErydCode <dyre.espana@gmail.com>
2023-05-14 14:13:44 +0200

    Remove not used implements

--- src/main/java/com/codecool/marsexploration/data/Area.java
+++ src/main/java/com/codecool/marsexploration/data/Area.java
@@ -1,4 +1,4 @@
 package com.codecool.marsexploration.data;
 
-public record Area(String name, int amountOfSymbols, String symbol, int[] rgb) implements SymbolColorProvider {
+public record Area(String name, int amountOfSymbols, String symbol, int[] rgb) {
 }
